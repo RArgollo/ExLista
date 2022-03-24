c0784ec3728ec088012d6f666c557a179fdd8c81
Argollo <rafaelargollo22@hotmail.com>
2022-03-23 22:08:03 -0300

    Explorando métodos de remoção Lists

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExLista;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var numeros = new List<int>();
+        var numerosMaisDoze = new List<int>();
+        var strings = new List<string>();
+        for (var contador = 0; contador < 10; contador++)
+        {
+            //Adicionar um valor dentro da lista
+            numeros.Add(contador+2);
+            numerosMaisDoze.Add(contador + 12);
+            strings.Add(contador.ToString());
+        }
+        //Adicinar uma lista dentro de outra
+        numeros.AddRange(numerosMaisDoze);
+        //Remover Item
+        numeros.Remove(5);
+        numeros.RemoveAt(2);
+    }
+}
