a684b4bcc41fb86b75a9986d44f7d074fae33547
Argollo <rafaelargollo22@hotmail.com>
2022-03-23 23:01:45 -0300

    Aprendendo novas funcionalidades da classe List

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ public class Program
         for (var contador = 0; contador < 10; contador++)
         {
             //Adicionar um valor dentro da lista
-            numeros.Add(contador+2);
+            numeros.Add(contador + 2);
             numerosMaisDoze.Add(contador + 12);
             strings.Add(contador.ToString());
         }
@@ -18,5 +18,21 @@ public class Program
         //Remover Item
         numeros.Remove(5);
         numeros.RemoveAt(2);
+        numeros.RemoveAll(numeroARemover => numeroARemover != 10); // referência do predicado
+        numeros.RemoveRange(0, 5); //a partir da posição, x itens (posição, QtdeItens)
+        //Buscar index de um valor
+        numeros.IndexOf(5); //busca a posição do valor informado
+        //Buscar um valor na lista
+        numeros.ElementAt(3); // busca o valor na posição informada
+        numeros.Find(numero => numero % 2 == 0); // busca o primeiro valor que atenda ao predicado
+        numeros.FindAll(numero => numero % 2 == 0); // busca todos os valores que atendam ao predicado e retorna uma lista
+        numeros.Where(numero => numero % 2 == 0); // ***
+        //Verificação
+        numeros.Any(numero => numero % 2 == 0); // confirmação de existência de qualquer item que atendam ao predicado
+        numeros.Exists(numero => numero % 2 == 0); // confirmação de existência de qualquer item que atendam ao predicado
+        numeros.All(numero => numero % 2 == 0); //verifica se todos os números atendem ao predicado
+        //Ordenação
+        numeros.OrderByDescending(x => x); // retorna uma lista de forma decrescente
+        numeros.OrderBy(x => x); // retorna uma lista de forma crescente
     }
 }
